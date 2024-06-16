010c0a14bf30e96a2340d10444ee5234a5ea04d8
박창준 <qmak01@naver.com>
2024-06-16 18:32:18 +0900

    [Design] Sidebar 및 Footer 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718088785',resources: [
+    cacheName: 'chirpy-1718530288',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
