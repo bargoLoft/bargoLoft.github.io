4f6628056551a4a03a012958c11082a308343e48
박창준 <qmak01@naver.com>
2024-07-08 23:53:01 +0900

    [Post] 2주차 자료구조

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718806874',resources: [
+    cacheName: 'chirpy-1720270750',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
